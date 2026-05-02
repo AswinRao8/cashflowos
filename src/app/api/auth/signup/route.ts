81e63e96eafc8b78bae5eca21bbe09377070173a
AswinRao8 <100790932+AswinRao8@users.noreply.github.com>
2026-05-02 14:11:55 +0400

    Update route.ts

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
+import { Prisma } from "@prisma/client";
 
 export async function POST(req: Request) {
   try {
